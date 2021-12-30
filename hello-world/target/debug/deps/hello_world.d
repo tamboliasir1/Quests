ae432d394a8e37178583e5e15f7b1ef503ecf6ee
Asir Tamboli <tamboliasir1@gmail.com>
2021-12-30 13:11:10 +0530

    Hello world Rust

---
+++ hello-world/target/debug/deps/hello_world.d
@@ -0,0 +1,5 @@
+D:\Rust\Quests\hello-world\target\debug\deps\hello_world.exe: src\main.rs
+
+D:\Rust\Quests\hello-world\target\debug\deps\hello_world.d: src\main.rs
+
+src\main.rs:
---
+++ hello-world/target/debug/hello-world.d
@@ -0,0 +1,1 @@
+D:\Rust\Quests\hello-world\target\debug\hello-world.exe: D:\Rust\Quests\hello-world\src\main.rs
