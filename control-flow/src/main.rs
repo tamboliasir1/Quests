5c6382ea145451fbe5a283e7f45aa3155eda2bab
Asir Tamboli <tamboliasir1@gmail.com>
2021-12-30 14:48:10 +0530

    Rust control flow

---
+++ control-flow/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let x=3;
+
+    if x==1{
+        println!("x=3");
+    }
+    else if x==2{
+        println!("x==2")
+    }
+    else{
+        println!("x=3")
+    }
+    }
+    
